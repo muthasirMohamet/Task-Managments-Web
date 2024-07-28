30deba67e8284ba1d7472672de61a4d9f4670269
Mudathir Mohamed <165424772+muthasirMohamet@users.noreply.github.com>
2024-07-28 16:14:23 +0300

    two files

---
+++ AddToPostgresDB/target/classes/com/example/StudentInfoDB/BootsprapExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootsprapExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootsprapExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootsprapExampleApplication.class, args);
+	}
+
+}
